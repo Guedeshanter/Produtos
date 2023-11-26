a8fc76e4426b30d38c84dc3ca0a50ac53302197f
Guedeshanter <149610617+Guedeshanter@users.noreply.github.com>
2023-11-26 17:46:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function  abrirNav(){
+document.getElementById("MenuOculto").style.width="250px";
+document.getElementById("principal").style.marginLeft="250px";
+}
+function fecharNav(){
+    document.getElementById("MenuOculto").style.width="0";
+document.getElementById("principal").style.marginLeft="0";
+}
